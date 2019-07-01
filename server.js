68b11634d54db3703d76146e5bd51238c2fa3100
bchang1213 <bchang952@gmail.com>
2019-06-30 23:02:26 -0700

    new changes

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
 import express from 'express';
 import http from 'http';
-import apiData from './apiData';
 const app = express();
 const server = http.createServer(app);
 const port = 8000;
-
-console.log("Server.js was started up...")
-console.log("apiData is as follows: " + JSON.stringify(apiData, null, 2));
-
-//If the apiData object does not contain the essential user information from Hackaday:
-if (!apiData.userKey || !apiData.clientId || !apiData.clientSecret) {
-    console.log('Please fill in your client data!  See line 10 in server.js.');
-    console.log('Ending node process.');
-    process.exit();
-}
 
 // Sets the location where express will look for the ejs views
 app.set('views', __dirname + '/views'); 

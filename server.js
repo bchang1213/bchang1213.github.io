1cc77d02f1c7090d0f18331cf85e5689909f4a90
Brian <bchang952@gmail.com>
2019-09-03 17:47:43 -0700

    db setup

--- server.js
+++ server.js
@@ -4,6 +4,25 @@ const app = express();
 const server = http.createServer(app);
 const port = 8000;
 var bodyParser = require('body-parser');
+var session = require('express-session');
+
+
+//Enable Session Authentication
+app.use(session({
+    secret: 'SuperSecratCodeChangeMeLatr',
+    resave: true,
+    saveUninitialized: true
+}));
+
+var auth = function(req, res, next) {
+    if(req.session && req.session.user === 'amy' && req.session.admin){
+        return next();
+    }
+    else{
+        return res.sendStatus(401);
+    }
+
+};
 
 //enable body parser
 app.use(bodyParser.json());
@@ -26,5 +45,44 @@ app.all('*', function (req, res) {
 
 //run the server on port 8000
 server.listen(process.env.PORT || port);
+server.on('error', onError);
+// server.on('listening', onListening);
 console.log('Listening on port: ', port);
 
+/*
+ * Event Listener for HTTP Server 'error' event
+ *
+*/
+ function onError(error) {
+     if(error.syscall !== 'listen') {
+        throw error;
+     }
+
+     const bind = typeof port ===  'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+     // handle specific listen errors with friendly messages
+     switch (error.code) {
+        case 'EACCES':
+             console.error(bind + ' requires elevated privileges.');
+             process.exit(1);
+             break;
+        default:
+             throw error;
+     }
+ }
+
+/**
+ * Event listener for HTTP server 'listening' event
+ *
+ * */
+
+// function onListening() {
+//     const addr = server.address();
+//     const bind = typeof addr === 'string'
+//         ? 'pipe' + addr
+//         : 'port' + addr.port;
+//     debug('Listening on ' + bind);
+
+// }
